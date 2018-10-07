59ca069a962618505922b4ae52550f6a2d74329f
Tim Powell <bangpow00@gmail.com>
2018-10-07 15:51:59 -0600

    Add project files.

---
+++ Beerio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Beerio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
