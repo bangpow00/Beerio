27f4f69a52bf8d58f3d00ccf00828687d4a858e0
Tim Powell <bangpow00@gmail.com>
2018-10-11 08:01:46 -0600

    delete the repo ideas. Not used in this EF version

--- Beerio.Tests/RecipeTests.cs
+++ Beerio.Tests/RecipeTests.cs
@@ -12,7 +12,7 @@ namespace Beerio.Tests
         {
             var recipe = new Recipe() { Name = "foo" };
             var ingredient = new Ingredient() { Name = "water" };
-            recipe.AddIngredient(ingredient, 1, Units.gal);
+            recipe.AddRecipeIngredient(ingredient, 1, Units.gal);
 
             var l = recipe.RecipeIngredients;
 
@@ -20,12 +20,29 @@ namespace Beerio.Tests
         }
 
         [TestMethod]
+        public void TestRecipeIngredients()
+        {
+            var recipe = new Recipe() { Name = "test 2" };
+            recipe.AddRecipeIngredient(new Ingredient() { Name = "water" },
+                                        1, Units.gal);
+            recipe.AddRecipeIngredient(new Ingredient() { Name = "hops" },
+                                       2, Units.cup);
+            recipe.AddRecipeIngredient(new Ingredient() { Name = "yeast" },
+                           5, Units.oz);
+
+            var l = recipe.RecipeIngredients;
+
+            Assert.IsTrue(l.Count == 3);
+        }
+
+
+        [TestMethod]
         [ExpectedException(typeof(System.ArgumentNullException))]
         public void TestNullIngredientException()
         {
             var recipe = new Recipe() { Name = "no ingredient" };
 
-            recipe.AddIngredient(null, 1, Units.oz);
+            recipe.AddRecipeIngredient(null, 1, Units.oz);
 
         }
 
@@ -36,7 +53,7 @@ namespace Beerio.Tests
             var recipe = new Recipe() { Name = "no quantity" };
             var ingredient = new Ingredient() { Name = "hops" };
 
-            recipe.AddIngredient(ingredient, 0, Units.lb);
+            recipe.AddRecipeIngredient(ingredient, 0, Units.lb);
 
         }
 
